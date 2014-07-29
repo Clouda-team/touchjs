44ffb9b2e179b73bf8a2e2bc28b8bfcf0f820392
afc163 <afc163@gmail.com>
2014-07-29 18:57:25 +0800

    Add spm support

--- touch.js
+++ touch.js
@@ -1,7 +1,9 @@
 /*! touchjs v0.2.13  2014-02-24 */
 'use strict';
 (function(root, factory) {
-    if (typeof define === 'function' && (define.amd || define.cmd)) {
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = factory();
+    } else if (typeof define === 'function' && (define.amd || define.cmd)) {
         define(factory); //Register as a module.
     } else {
         root.touch = factory();
